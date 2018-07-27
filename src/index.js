746a756fef1c8ff41fcae3f6c1f8cfd573e92125
Jerome Olvera <jerome.olvera@gmail.com>
2018-07-27 14:16:38 -0500

    fixed favicon

--- src/index.js
+++ src/index.js
@@ -19,6 +19,6 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>
-  , document.getElementById('root'));
-/*registerServiceWorker();*/
+  </ApolloProvider>,
+  document.getElementById('root')
+);
