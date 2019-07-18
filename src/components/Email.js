deb2ba026a9160894fffdc0a613785c1cd69de2f
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2019-07-18 11:57:46 -0500

    Added try-catch block to see if email exists

--- src/components/Email.js
+++ src/components/Email.js
@@ -4,56 +4,63 @@ import gql from 'graphql-tag'
 
 
 class Email extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-  this.state = {
-    email: '',
-    show: false,
+    this.state = {
+      email: '',
+      show: false,
+    }
   }
-}
 
   render() {
     return (
       <div>
-      <h1>Forgot Password?</h1>
-      <form className="form" id="formulario" onSubmit={this._createLink.bind(this)}>
-        <fieldset>
-          <div className=" form-group">
-            <div className="input-group">
-              <p>Type your email</p>
-              <input autoComplete="off" id="passInput" type="email" className="form-control" onChange={(e)=> this.setState({email: e.target.value})}/>
-              {
-                this.state.show
-                  ? <div style={{ textAlign: 'center' }} className="alert alert-danger">This email doesn´t exist </div>
-                  : null
-              }
+        <h1>Forgot Password?</h1>
+        <form className="form" id="formulario" onSubmit={this._createLink.bind(this)}>
+          <fieldset>
+            <div className=" form-group">
+              <div className="input-group">
+                <p>Type your email</p>
+                <input autoComplete="off" id="passInput" type="email" className="form-control" onChange={(e) => this.setState({ email: e.target.value })} />
+                {
+                  this.state.show
+                    ? <div style={{ textAlign: 'center' }} className="alert alert-danger">This email doesn´t exist </div>
+                    : null
+                }
+              </div>
             </div>
-          </div>
-          <div className="form-group">
-            <input className="btn btn-lg btn-primary btn-block" value="Verify Email" type="submit"/>
-          </div>
-    </fieldset>
-    </form>
-    </div>
+            <div className="form-group">
+              <input className="btn btn-lg btn-primary btn-block" value="Verify Email" type="submit" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
     )
   }
 
   _createLink = async (e) => {
     e.preventDefault();
-  const email  = this.state.email;
-  const result = await this.props.resetPassword({
-    variables: {
-      email
+    const email = this.state.email;
+    try {
+      const result = await this.props.resetPassword({
+        variables: {
+          email
+        }
+      });
+
+      if (result.data.sendResetPassword) {
+        this.props.history.push('/confirm');
+      } else {
+        document.getElementById('passInput').value = "";
+        this.setState({ show: true });
+      }
+
+    } catch (error) {
+      document.getElementById('passInput').value = "";
+      this.setState({ show: true });
     }
-  });
-  if (result.data.sendResetPassword) {
-    this.props.history.push('/confirm');
+
   }
-  else {
-    document.getElementById('passInput').value="";
-    this.setState({show: true});
-  }
-}
 
 }
 
